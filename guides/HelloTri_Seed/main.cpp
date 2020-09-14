2c49e27b076eac04948e76073961da9b314767ca
Ed94 <edwardgz@gmail.com>
2020-09-14 15:26:36 -0400

    Got Tri seed to build with xwin on msvc.. (clang-cl wont tho)

--- guides/HelloTri_Seed/main.cpp
+++ guides/HelloTri_Seed/main.cpp
@@ -1,11 +1,59 @@
 #include "CrossWindow/CrossWindow.h"
 #include "Renderer.hpp"
 
+#include <iostream>
 
 
-void xmain()
+
+void xmain(int argc, const char** argv)
 {
-    int one = 1; 
+    // 🖼 Create Window
+    xwin::WindowDesc wdesc;
+    wdesc.title = "VT Seed: Hello Triangle";
+    wdesc.name = "MainWindow";
+    wdesc.visible = true;
+    wdesc.width = 640;
+    wdesc.height = 64;
+    wdesc.fullscreen = false;
 
+    xwin::Window window;
+    xwin::EventQueue eventQueue;
 
+    if (!window.create(wdesc, eventQueue))
+    { return; }
+
+    // Engine Loop
+    bool isRunning = true;
+
+    while (isRunning)
+    {
+        // bool shouldRender = true;
+
+        eventQueue.update();
+
+        while (!eventQueue.empty())
+        {
+            const xwin::Event& event = eventQueue.front();
+
+            if (event.type == xwin::EventType::Resize)
+            {
+                const xwin::ResizeData data = event.data.resize;
+
+                // renderer.resize(data.width, data.height);
+
+                // shouldRender = false;                
+            }
+
+            if (event.type == xwin::EventType::Close)
+            {
+                window.close();
+
+                // shouldRender = false;
+
+                isRunning = false;
+            }
+
+            eventQueue.pop();
+        }        
+    }
 }
